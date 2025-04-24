888b7a260d162cb36cc0c3bdf67ee54bc8198e96
Premium07 <premsagarg23@gmail.com>
2025-04-24 17:41:31 +0545

    feat: update index.html title and enhance App component layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="h-screen w-full p-5 flex items-center gap-4 bg-zinc-900">
+      <section className="h-full  basis-1/2 bg-zinc-800 rounded relative">
+        <div></div>
+        <button className="absolute bottom-4 right-4 py-2 px-8 rounded cursor-pointer text-zinc-200 font-semibold bg-zinc-900">
+          Review
+        </button>
+      </section>
+      <section className="h-full flex basis-1/2 bg-zinc-500 rounded"></section>
+    </main>
+  );
 };
 
 export default App;
