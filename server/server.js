0f06191c35b6f6dda49e5fc001cffa066d1109e8
Premium07 <premsagarg23@gmail.com>
2025-04-24 14:26:06 +0545

    feat: add Google GenAI service and update dependencies

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 import { configDotenv } from "dotenv";
 import app from "./src/app.js";
 configDotenv();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
