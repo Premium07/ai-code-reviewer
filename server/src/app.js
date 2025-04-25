b39a64634786593fb628dd83629f14a77028fa51
Premium07 <premsagarg23@gmail.com>
2025-04-25 15:49:15 +0545

    feat: add cron job for periodic API requests and update package dependencies

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,19 @@
 import express from "express";
 import aiRoutes from "./routes/ai.routes.js";
 import cors from "cors";
+import job from "./config/cron.js";
+
+job.start();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the server" });
+});
 
 app.use("/ai", aiRoutes);
 
